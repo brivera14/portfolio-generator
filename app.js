ed1b7957184bf7562a9591b7a758511d95fb7179
Bryan Rivera Gonzalez <rivera.bryan1406@gmail.com>
2020-08-06 05:08:35 -0400

    Html File generated

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
+// // console.log(profileDataArgs);
 
-    console.log('===================');
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//     console.log('===================');
 
-printProfileData(profileDataArgs);
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+
+// console.log(name, github);
+// console.log(generatepage(name, github));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
