a512eb62b8e8f6eb222d73381669455cbcd99eaf
Bryan Rivera Gonzalez <rivera.bryan1406@gmail.com>
2020-08-24 19:42:27 -0400

    Issue 4 resolved, now we can add different projects and filter it whether is featured or no

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // const name = profileDataArgs[0];
@@ -160,5 +160,14 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
+    // console.log(portfolioData);
   });
+
+// const pageHTML = generatePage(mockData);
